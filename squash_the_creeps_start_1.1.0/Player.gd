40be17b313e863ea1d77ad75d1d051b48415a707
roojask <thaninrat.l@kkumail.com>
2025-09-11 23:45:46 +0700

    UserInterface

--- squash_the_creeps_start_1.1.0/Player.gd
+++ squash_the_creeps_start_1.1.0/Player.gd
@@ -4,13 +4,17 @@ extends CharacterBody3D
 @export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
-
+# Vertical impulse applied to the character upon jumping in meters per second.
+@export var jump_impulse = 20
 var target_velocity = Vector3.ZERO
-
+# Vertical impulse applied to the character upon bouncing over a mob in
+# meters per second.
+@export var bounce_impulse = 16
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
-
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -24,7 +28,29 @@ func _physics_process(delta):
 		direction = direction.normalized()
 		# Setting the basis property will affect the rotation of the node.
 		$Pivot.basis = Basis.looking_at(direction)
+# Iterate through all collisions that occurred this frame
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
 
+		# If there are duplicate collisions with a mob in a single frame
+		# the mob will be deleted after the first collision, and a second call to
+		# get_collider will return null, leading to a null pointer when calling
+		# collision.get_collider().is_in_group("mob").
+		# This block of code prevents processing duplicate collisions.
+		if collision.get_collider() == null:
+			continue
+
+		# If the collider is with a mob
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			# we check that we are hitting it from above.
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				# If so, we squash it and bounce.
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				# Prevent further duplicate calls.
+				break
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
@@ -36,3 +62,16 @@ func _physics_process(delta):
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
+
+signal hit
+func _on_mob_detector_body_entered(body: Node3D) -> void:
+	die()# Replace with function body.
+# Emitted when the player was hit by a mob.
+# Put this at the top of the script.
+# And this function at the bottom.
+func die():
+	hit.emit()
+	queue_free()
+
+func _on_player_hit():
+	$MobTimer.stop()
